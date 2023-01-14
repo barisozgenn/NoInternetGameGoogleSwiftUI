4eeb02774b22de9b2e3397f4f20f832429d4e27b
Barış Özgen <barisozgen@hotmail.com>
2023-01-14 10:28:08 +0400

    assets and targets created

---
+++ NoInternetGoogleGameSwiftUI/Game-iOS/Game_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_iOSApp.swift
+//  Game-iOS
+//
+//  Created by Baris OZGEN on 14.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Game_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
