ce93531222116bbf6b972cc9fd5fae68e8f7ab42
Barış Özgen <barisozgen@hotmail.com>
2023-01-14 11:39:51 +0400

    content demo view created

--- NoInternetGoogleGameSwiftUI/Game-iOS/ContentView.swift
+++ NoInternetGoogleGameSwiftUI/Game-iOS/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var dinoCurrentImage = UIImage(named: "dino-idle")!
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Image("cloud")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 29)
+                .offset(x: -92, y: -229)
+            Image("cloud")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 29)
+                .offset(x: 72, y: -129)
+            
+            Text("hello 00000")
+                .font(.custom("PressStart2P", size: 29))
+            
+            Image("obstacle-1")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 92)
+                .offset(x: 72, y: -7)
+            VStack {
+                Image(uiImage: dinoCurrentImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxHeight: 107)
+                    .offset(x: -107)
+                Image("way")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 2900)
+                    .offset(y: -34)
+            }
         }
-        .padding()
     }
 }
 
