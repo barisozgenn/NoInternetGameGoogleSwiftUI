6b9e8da4edd3f993d486461b0c73100c04adff2f
Barış Özgen <barisozgen@hotmail.com>
2023-01-27 23:25:24 +0400

    views separated

--- NoInternetGoogleGameSwiftUI/Game-iOS/Core/ObstacleView.swift
+++ NoInternetGoogleGameSwiftUI/Game-iOS/Core/ObstacleView.swift
@@ -6,46 +6,72 @@
 //
 import SwiftUI
 struct ObstacleView : View {
-    let obstacleList =  ObstacleModel.allCases
     @State private var posX : Double = 0
-    @State private var maxX: Double = 400
-    @State private var minX: Double = -400
-    @State var speed: Double = 12
+    @State private var maxX: Double = 500
+    @State private var minX: Double = -500
+    @State var speed: Double = 15.0
+    let range = 92.0...192
+    @State var newText = ""
+    @State var changeIt = false
+    let timer = Timer.publish(every: 0.009, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        GeometryReader { geo in
-            
-            HStack(alignment: .bottom){
-                Image(obstacleList[obstacleList.indices.randomElement()!].imageName)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: 72)
+           
                 ZStack{
-                    Rectangle()
-                        .frame(width: 29,height: 192)
+                    ObstaclePrefab(changeIt: $changeIt)
+                        .position(x: posX, y: 96)
+                    ObstaclePrefab(changeIt: $changeIt)
+                        .position(x: posX + 258, y: 96)
+                    ObstaclePrefab(changeIt: $changeIt)
+                        .position(x: posX + 592, y: 96)
+                    ObstaclePrefab(changeIt: $changeIt)
+                        .position(x: posX + 900, y: 96)
+                    Text("\(newText)")
+                }
+                .onAppear{
+                    changeIt.toggle()
+                    posX = maxX
+                   
+                }
+                .onReceive(timer) { _ in
+                    posX -= 1
+                    newText = "\(posX)"
                     
+                    if posX < -990 {
+                        posX = maxX
+                    }
                 }
-            }
-            .background(.yellow)
-            .onAppear{
-                maxX = geo.size.width + 129
-                minX = maxX * -1
-                posX = maxX
-                
-                withAnimation(.linear(duration: speed).repeatForever(autoreverses: false)){
-                    posX = minX
-                }
-            }
-            .offset(x: posX)
-            
-            Text("posX:\(posX)")
-                .foregroundColor(.white)
-        }
-        .frame(height: 192)
-        .background(.red)
+        .frame(width: 430,height: 192)
+        .clipped()
+        //.background(.red)
         
     }
 }
 
+private struct ObstaclePrefab: View {
+    @Binding var changeIt : Bool
+    let obstacleList =  ObstacleModel.allCases
+@State private var image = ""
+    var body: some View {
+            HStack(alignment: .bottom){
+                Image(image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 72,height: 72)
+                ZStack{
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(width: 29,height: 192)
+                        
+                    
+                }
+            }
+            //.background(.yellow)
+            .onChange(of: changeIt, perform: { _ in
+                image = obstacleList[obstacleList.indices.randomElement()!].imageName
+            })
+    }
+}
 struct ObstacleView_Previews: PreviewProvider {
     static var previews: some View {
         ObstacleView().offset(x: 0, y: 0)
