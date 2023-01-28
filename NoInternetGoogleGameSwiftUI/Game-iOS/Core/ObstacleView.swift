ab0f8f17b58ff68576c00fb4427257b7c915ac1c
Barış Özgen <barisozgen@hotmail.com>
2023-01-28 15:42:06 +0400

    obstacle problem fixed

--- NoInternetGoogleGameSwiftUI/Game-iOS/Core/ObstacleView.swift
+++ NoInternetGoogleGameSwiftUI/Game-iOS/Core/ObstacleView.swift
@@ -7,35 +7,36 @@
 import SwiftUI
 struct ObstacleView : View {
     @State private var posX : Double = 0
+    @State private var posXs : [Double] = [0.0,0.0,0.0,0]
     @State private var maxX: Double = 500
     @State private var minX: Double = -500
     @State var speed: Double = 15.0
     let range = 92.0...192
-    @State var newText = ""
     @State var changeIt = false
+    @State var colliderHit = false
+    @State var getScore = false
     let timer = Timer.publish(every: 0.009, on: .main, in: .common).autoconnect()
     
     var body: some View {
            
                 ZStack{
-                    ObstaclePrefab(changeIt: $changeIt)
-                        .position(x: posX, y: 96)
-                    ObstaclePrefab(changeIt: $changeIt)
-                        .position(x: posX + 258, y: 96)
-                    ObstaclePrefab(changeIt: $changeIt)
-                        .position(x: posX + 592, y: 96)
-                    ObstaclePrefab(changeIt: $changeIt)
-                        .position(x: posX + 900, y: 96)
-                    Text("\(newText)")
+                    ObstaclePrefab(changeIt: $changeIt, posX: $posXs[0], colliderHit: $colliderHit, getScore: $getScore)
+                        .position(x: posXs[0], y: 96)
+                    ObstaclePrefab(changeIt: $changeIt, posX: $posXs[1], colliderHit: $colliderHit, getScore: $getScore)
+                        .position(x: posXs[1], y: 96)
+                    ObstaclePrefab(changeIt: $changeIt, posX: $posXs[2], colliderHit: $colliderHit, getScore: $getScore)
+                        .position(x: posXs[2], y: 96)
+                    ObstaclePrefab(changeIt: $changeIt, posX: $posXs[3], colliderHit: $colliderHit, getScore: $getScore)
+                        .position(x: posXs[3], y: 96)
                 }
                 .onAppear{
                     changeIt.toggle()
                     posX = maxX
-                   
+                   posXs = [posX, posX+258, posX+592, posX+900]
                 }
                 .onReceive(timer) { _ in
                     posX -= 1
-                    newText = "\(posX)"
+                    posXs = [posX, posX+258, posX+592, posX+900]
                     
                     if posX < -990 {
                         posX = maxX
@@ -50,6 +51,9 @@ struct ObstacleView : View {
 
 private struct ObstaclePrefab: View {
     @Binding var changeIt : Bool
+    @Binding var posX : Double
+    @Binding var colliderHit : Bool
+    @Binding var getScore : Bool
     let obstacleList =  ObstacleModel.allCases
 @State private var image = ""
     var body: some View {
@@ -62,8 +66,7 @@ private struct ObstaclePrefab: View {
                     Rectangle()
                         .foregroundColor(.clear)
                         .frame(width: 29,height: 192)
-                        
-                    
+                      Text("\(String(format: "%.0f",posX))")
                 }
             }
             //.background(.yellow)
