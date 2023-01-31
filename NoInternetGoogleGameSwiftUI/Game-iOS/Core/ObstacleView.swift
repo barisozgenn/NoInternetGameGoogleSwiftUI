09f520721fc901c0f41bc22c8223534dfc18090b
Barış Özgen <barisozgen@hotmail.com>
2023-01-31 11:33:54 +0400

    jump and obstacle movements done

--- NoInternetGoogleGameSwiftUI/Game-iOS/Core/ObstacleView.swift
+++ NoInternetGoogleGameSwiftUI/Game-iOS/Core/ObstacleView.swift
@@ -19,36 +19,40 @@ struct ObstacleView : View {
     let timer = Timer.publish(every: 0.007, on: .main, in: .common).autoconnect()
     
     var body: some View {
-           
-                ZStack{
-                    ObstaclePrefab(changeIt: $changeIt, posX: $posXs[0], colliderHit: $colliderHit, getScore: $getScore)
-                        .position(x: posXs[0], y: 96)
-                    ObstaclePrefab(changeIt: $changeIt, posX: $posXs[1], colliderHit: $colliderHit, getScore: $getScore)
-                        .position(x: posXs[1], y: 96)
-                    ObstaclePrefab(changeIt: $changeIt, posX: $posXs[2], colliderHit: $colliderHit, getScore: $getScore)
-                        .position(x: posXs[2], y: 96)
-                    ObstaclePrefab(changeIt: $changeIt, posX: $posXs[3], colliderHit: $colliderHit, getScore: $getScore)
-                        .position(x: posXs[3], y: 96)
-                    
-                    Text("\(posXs[0])\n\(posXs[1])\n\(posXs[2])\n\(posXs[3])\n")
-                }
-                .onAppear{
-                    changeIt.toggle()
+        
+        ZStack{
+            ObstaclePrefab(changeIt: $changeIt, posX: $posXs[0], colliderHit: $colliderHit, getScore: $getScore)
+                .position(x: posXs[0], y: 96)
+            ObstaclePrefab(changeIt: $changeIt, posX: $posXs[1], colliderHit: $colliderHit, getScore: $getScore)
+                .position(x: posXs[1], y: 96)
+            ObstaclePrefab(changeIt: $changeIt, posX: $posXs[2], colliderHit: $colliderHit, getScore: $getScore)
+                .position(x: posXs[2], y: 96)
+            ObstaclePrefab(changeIt: $changeIt, posX: $posXs[3], colliderHit: $colliderHit, getScore: $getScore)
+                .position(x: posXs[3], y: 96)
+            
+            /*Text("\(posXs[0])\n\(posXs[1])\n\(posXs[2])\n\(posXs[3])\n")*/
+        }
+        .onAppear{
+            changeIt.toggle()
+            posX = maxX
+            posXs = [posX, posX+258, posX+592, posX+900]
+        }
+        .onReceive(timer) { _ in
+            
+            if isGameStart && !colliderHit {
+                
+                posX -= 1
+                posXs = [posX, posX+258, posX+592, posX+900]
+                
+                if posX < -990 {
                     posX = maxX
-                   posXs = [posX, posX+258, posX+592, posX+900]
                 }
-                .onReceive(timer) { _ in
-                    
-                    if isGameStart && !colliderHit {
-                        
-                        posX -= 1
-                        posXs = [posX, posX+258, posX+592, posX+900]
-                        
-                        if posX < -990 {
-                            posX = maxX
-                        }
-                    }
-                }
+            }
+        }
+        .onChange(of: colliderHit, perform: { newValue in
+            posX = maxX
+            posXs = [posX, posX+258, posX+592, posX+900]
+        })
         .frame(width: 430,height: 192)
         .clipped()
         //.background(.red)
@@ -62,31 +66,31 @@ private struct ObstaclePrefab: View {
     @Binding var colliderHit : Bool
     @Binding var getScore : Int
     let obstacleList =  ObstacleModel.allCases
-@State private var image = ""
+    @State private var image = ""
     var body: some View {
-            HStack(alignment: .bottom){
-                Image(image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 72,height: 72)
-                ZStack{
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: 29,height: 192)
-                      Text("\(String(format: "%.0f",posX))")
+        HStack(alignment: .bottom){
+            Image(image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 72,height: 72)
+            ZStack{
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 29,height: 192)
+                //Text("\(String(format: "%.0f",posX))")
+            }
+        }
+        //.background(.yellow)
+        .onChange(of: changeIt, perform: { _ in
+            image = obstacleList[obstacleList.indices.randomElement()!].imageName
+        })
+        .onChange(of: posX) { newPosX in
+            if !colliderHit && posX == 29 {
+                withAnimation(.spring()){
+                    getScore += 14
                 }
             }
-            //.background(.yellow)
-            .onChange(of: changeIt, perform: { _ in
-                image = obstacleList[obstacleList.indices.randomElement()!].imageName
-            })
-            .onChange(of: posX) { newPosX in
-                if !colliderHit && posX == 29 {
-                    withAnimation(.spring()){
-                        getScore += 14
-                    }
-                }
-            }
+        }
     }
 }
 struct ObstacleView_Previews: PreviewProvider {
