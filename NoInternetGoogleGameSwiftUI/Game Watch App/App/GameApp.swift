c4476b246d6e2f49900b47b2fdf80db9c4b75627
Barış Özgen <barisozgen@hotmail.com>
2023-02-07 19:22:18 +0400

    watchOS version is done

---
+++ NoInternetGoogleGameSwiftUI/Game Watch App/App/GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameApp.swift
+//  Game Watch App
+//
+//  Created by Baris OZGEN on 7.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Game_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            GameView()
+        }
+    }
+}
--- NoInternetGoogleGameSwiftUI/Game Watch App/Core/GameView.swift
+++ NoInternetGoogleGameSwiftUI/Game Watch App/Core/GameView.swift
@@ -1,32 +1,40 @@
 //
 //  GameView.swift
-//  Game-watchOS
+//  Game Watch App
 //
-//  Created by Baris OZGEN on 6.02.2023.
+//  Created by Baris OZGEN on 7.02.2023.
 //
 
 import SwiftUI
 
 struct GameView: View {
-   
+    
     @State private var score = 0
     @State var getScore = 0
     @State var isGameStart : Bool = false
     @State var dinoPosY = 0.0
     @State var dinoState : DinoStateModel = .walk
     @State var colliderHit = false
+    @State private var playLabelOpacity = 1.0
+    
     var body: some View {
         ZStack{
             CloudsView()
+                .offset(y: 114)
             ObstacleView(colliderHit: $colliderHit, isGameStart: $isGameStart, getScore: $getScore, dinoPosY: $dinoPosY, dinoState: $dinoState)
-                .offset(y: -39)
+                .offset(y: 0)
             scoreLabel
+                .offset(y: -58)
             
             GroundView(dinoState: $dinoState)
-                .offset(y: 39)
+                .offset(y: 72)
             DinoView(dinoPosY: $dinoPosY, dinoState: $dinoState, isGameStart: $isGameStart)
+                .offset(y: 42)
             replayButton
+                .offset(y: -29)
+            playLabel
         }
+        .scaleEffect(0.5)
     }
 }
 extension GameView {
@@ -66,16 +74,30 @@ extension GameView {
                             .fontWeight(.bold)
                     }
                 }
+                .padding()
             }
         }
-        
-
     }
     
+    private var playLabel: some View {
+        VStack{
+            
+            Text("Tap Space".uppercased())
+            Text("to Play")
+                .font(.title2)
+                .foregroundColor(.gray)
+                .fontWeight(.bold)
+        }
+        .opacity(playLabelOpacity)
+        .onAppear{
+            withAnimation(.spring().delay(2)){
+                playLabelOpacity = 0.0
+            }
+        }
+    }
 }
 struct GameView_Previews: PreviewProvider {
     static var previews: some View {
         GameView()
     }
 }
-
